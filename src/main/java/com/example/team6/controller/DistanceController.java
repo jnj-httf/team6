9603036af711833ae16ea9da0b1c41f7d368b445
Guilherme Oliveira <gsoliveira@fitec.org.br>
2019-02-16 11:39:54 -0200

    Thymeleaf implemented

---
+++ src/main/java/com/example/team6/controller/DistanceController.java
@@ -0,0 +1,33 @@
+package com.example.team6.controller;
+
+import com.example.team6.model.Distance;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Log
+@RequestMapping("distancia")
+@Controller
+public class DistanceController
+{
+    @GetMapping
+    public String welcome(Model model)
+    {
+        model.addAttribute("message", "Oi");
+        model.addAttribute("distance", new Distance());
+
+        return "distance/distance";
+    }
+
+    @PostMapping
+    public String calcDistance(@ModelAttribute Distance distance,  Model model)
+    {
+        model.addAttribute("message", "Oi");
+        log.info(distance.toString());
+        return "distance/distance";
+    }
+}
---
+++ src/main/java/com/example/team6/model/Distance.java
@@ -0,0 +1,10 @@
+package com.example.team6.model;
+
+import lombok.Data;
+
+@Data
+public class Distance
+{
+    private String lat;
+    private String lng;
+}
